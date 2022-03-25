84df09215307e522c915ed0eb0928c2007b234f5
Vahtee Boo <vahtee.boo@gmail.com>
2022-03-25 11:17:18 +0200

    Initial Commit

---
+++ LocalNotofocationSTBC/LocalNotofocationSTBCApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalNotofocationSTBCApp.swift
+//  LocalNotofocationSTBC
+//
+//  Created by Vahtee Boo on 25.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct LocalNotofocationSTBCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
